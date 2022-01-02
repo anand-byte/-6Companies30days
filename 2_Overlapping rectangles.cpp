94b52a94960b05c3d44e95aeaf7296016e78a180
Abhishek Anand <63868642+anand-byte@users.noreply.github.com>
2022-01-02 20:25:36 +0530

    2_Overlapping rectangles.cpp

---
+++ 2_Overlapping rectangles.cpp
@@ -0,0 +1,6 @@
+//  https://practice.geeksforgeeks.org/problems/overlapping-rectangles1924/1/
+int doOverlap(int L1[], int R1[], int L2[], int R2[]) {
+        // code here
+        if(L1[0]>R2[0]||R1[1]>L2[1]||R1[0]<L2[0]||R2[1]>L1[1])
+            return 0;
+        return 1;
